d74c9e4d6c06022d5cc624999aa92a9409dabd67
lumos309 <35829443+lumos309@users.noreply.github.com>
2021-11-02 20:43:05 +0800

    Add MLP layer combining feats and probs

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+from typing import final
 from utils import *     # bad practice, nvm
 
 from sklearn.metrics import accuracy_score
@@ -25,7 +26,7 @@ def train_model(model, train_loader, test_loader, criterion, scheduler, optimize
             x, y = x.cuda(), y.cuda()
             pred = model(x)
             if epoch == num_epochs - 1: # for feature ensemble prediction
-                final_train_preds = pred
+                final_train_preds = pred if final_train_preds == None else torch.cat((final_train_preds, pred), 0)
             loss = criterion(pred, y.long())      # target must be of dtype = Long!
             train_acc.update(((pred.argmax(1) == y).sum() / len(y)).item())     # .item() discard gradient
             train_loss.update(loss.item())
@@ -46,7 +47,7 @@ def train_model(model, train_loader, test_loader, criterion, scheduler, optimize
                 x, y = x.cuda(), y.cuda()
                 pred = model(x)
                 if epoch == num_epochs - 1: # for feature ensemble prediction
-                    final_test_preds = pred
+                    final_test_preds = pred if final_test_preds == None else torch.cat((final_test_preds, pred), 0)
                 test_acc.update(((pred.argmax(1) == y).sum() / len(y)).item())     # .item() discard gradient
 
                 pg.set_postfix({
@@ -111,7 +112,7 @@ def train_bert(nlp_train, nlp_test, return_features=True):
     test_x, test_y = nlp_test['content'].tolist(), nlp_test['Target'].tolist()
 
     # training setup
-    num_epochs, base_lr, base_bs, ngpus = 1, 2e-5, 64, 1
+    num_epochs, base_lr, base_bs, ngpus = 5, 2e-5, 64, 1
     out_dim = max(test_y) + 1
     model = BertClassifier(extractor, LogisticRegression(768 * 128, 128, out_dim, dropout=0.3))
     model = nn.DataParallel(model).cuda()
@@ -130,6 +131,7 @@ def train_bert(nlp_train, nlp_test, return_features=True):
     final_test_acc = None
     final_train_preds, final_test_preds = None, None
     train_feats, test_feats = None, None
+
     for epoch in range(num_epochs):
         train_acc = AverageMeter()
         train_loss = AverageMeter()
@@ -139,7 +141,10 @@ def train_bert(nlp_train, nlp_test, return_features=True):
             x, y = (x1.cuda(), x2.cuda()), y.cuda()
             pred = model(x)
             if epoch == num_epochs - 1: # for feature ensemble prediction
-                final_train_preds, train_feats = model(x, return_feat=True)
+                p, f = model(x, return_feat=True)
+                f = torch.flatten(f.last_hidden_state, start_dim=1)
+                train_feats = f if (train_feats == None) else torch.cat((train_feats, f), 0)
+                final_train_preds = p if (final_train_preds == None) else torch.cat((final_train_preds, p), 0)
             loss = criterion(pred, y.long())
             train_acc.update(((pred.argmax(1) == y).sum() / len(y)).item())
             train_loss.update(loss.item())
@@ -161,7 +166,10 @@ def train_bert(nlp_train, nlp_test, return_features=True):
                 x, y = (x1.cuda(), x2.cuda()), y.cuda()
                 pred = model(x)
                 if epoch == num_epochs - 1: # for feature ensemble prediction
-                    final_test_preds, test_feats = model(x, return_feat=True)
+                    p, f = model(x, return_feat=True)
+                    f = torch.flatten(f.last_hidden_state, start_dim=1)
+                    test_feats = f if (test_feats == None) else torch.cat((test_feats, f), 0)
+                    final_test_preds = p if (final_test_preds == None) else torch.cat((final_test_preds, p), 0)
                 test_acc.update(((pred.argmax(1) == y).sum() / len(y)).item())
 
                 pg.set_postfix({
@@ -174,6 +182,7 @@ def train_bert(nlp_train, nlp_test, return_features=True):
         print(f'epoch {epoch}, train acc {train_acc.avg}, test acc {test_acc.avg}')
         final_test_acc = test_acc.avg
 
+    print(train_feats.shape)
     if return_features:
         return final_test_acc, final_train_preds, final_test_preds, train_feats, test_feats
     return final_test_acc, final_train_preds, final_test_preds
@@ -216,7 +225,7 @@ def train_style_based(nlp_train, nlp_test, return_features=False):
     del train_loader, test_loader
     
     if return_features:
-        return score_style, style_prob_train, style_prob_test, train_x, train_y
+        return score_style, style_prob_train, style_prob_test, train_x, test_x
     return score_style, style_prob_train, style_prob_test
 
 
@@ -251,7 +260,7 @@ def train_char_ngram(nlp_train, nlp_test, list_bigram, list_trigram, return_feat
     del train_loader, test_loader
 
     if return_features:
-        return final_test_acc, final_train_preds, final_test_preds, train_x, train_y
+        return final_test_acc, final_train_preds, final_test_preds, train_x, test_x
     return final_test_acc, final_train_preds, final_test_preds
 
 
@@ -277,21 +286,69 @@ def run_iterations(source):
         # score_lr = train_tf_idf(nlp_train, nlp_test)
         # print("Training done, accuracy is : ", score_lr)
 
-        # # Character N-gram only
-        score_char, char_prob_train, char_prob_test, char_feat_train, char_feat_test = train_char_ngram(nlp_train, nlp_test, list_bigram, list_trigram, return_features=True)
-        print("Training done, accuracy is : ", score_char)
+        # # Style-based classifier
+        score_style, style_prob_train, style_prob_test, style_feat_train, style_feat_test = train_style_based(nlp_train, nlp_test, return_features=True)
+        print("Training done, accuracy is : ", score_style)
+        print(style_prob_train.shape)
+        print(style_prob_test.shape)
+        print(style_feat_train.shape)
+        print(style_feat_test.shape)
 
         # Bert + Classification Layer
         score_bert, bert_prob_train, bert_prob_test, bert_feat_train, bert_feat_test = train_bert(nlp_train, nlp_test, return_features=True)
         print("Training done, accuracy is : ", score_bert)
+        print(bert_prob_train.shape)
+        print(bert_prob_test.shape)
+        print(bert_feat_train.shape)
+        print(bert_feat_test.shape)
 
-        # # Style-based classifier
-        score_style, style_prob_train, style_prob_test, style_feat_train, style_feat_test = train_style_based(nlp_train, nlp_test, return_features=True)
-        print("Training done, accuracy is : ", score_style)
+        # # Character N-gram only
+        score_char, char_prob_train, char_prob_test, char_feat_train, char_feat_test = train_char_ngram(nlp_train, nlp_test, list_bigram, list_trigram, return_features=True)
+        print("Training done, accuracy is : ", score_char)
+        print(char_prob_train.shape)
+        print(char_prob_test.shape)
+        print(char_feat_train.shape)
+        print(char_feat_test.shape)
 
         # # BERT + Style + Char N-gram
-        # print("#####")
-        # print("BERT + Style + Char N-gram")
+        print("#####")
+        print("BERT + Style + Char N-gram")
+
+        bert_prob_train = bert_prob_train.cpu().detach().numpy()
+        bert_feat_train = bert_feat_train.cpu().detach().numpy()
+        style_prob_train = style_prob_train.cpu().detach().numpy()
+        char_prob_train = char_prob_train.cpu().detach().numpy()
+
+        bert_prob_test = bert_prob_test.cpu().detach().numpy()
+        bert_feat_test = bert_feat_test.cpu().detach().numpy()
+        style_prob_test = style_prob_test.cpu().detach().numpy()
+        char_prob_test = char_prob_test.cpu().detach().numpy()
+        
+        # print(bert_prob_train.shape)
+        # print(bert_feat_train.shape)
+
+        ensemble_train_inputs = np.concatenate([bert_prob_train, bert_feat_train, style_prob_train, style_feat_train, char_prob_train, char_feat_train], axis=1)
+        ensemble_test_inputs = np.concatenate([bert_prob_test, bert_feat_test, style_prob_test, style_feat_test, char_prob_test, char_feat_test], axis=1)
+
+        print(ensemble_train_inputs.shape)
+        print(ensemble_test_inputs.shape)
+
+        train_y, test_y = nlp_train['Target'].to_numpy(), nlp_test['Target'].to_numpy()
+
+        num_epochs, base_lr, base_bs, ngpus = 100, 5e-4, 32, 1
+        in_dim, out_dim = ensemble_train_inputs.shape[1], test_y.max()+1
+        model = LogisticRegression(in_dim=in_dim, hid_dim=128, out_dim=out_dim, dropout=0.2)
+        optimizer = torch.optim.AdamW(params=model.parameters(), lr=base_lr*ngpus, weight_decay=1e-4)
+        criterion = nn.CrossEntropyLoss()
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+        train_set, test_set = NumpyDataset(ensemble_train_inputs, train_y), NumpyDataset(ensemble_test_inputs, test_y)
+        train_loader = DataLoader(train_set, batch_size=base_bs*ngpus, shuffle=True, num_workers=0, pin_memory=True)
+        test_loader = DataLoader(test_set, batch_size=base_bs*ngpus, shuffle=False, num_workers=0, pin_memory=True)
+
+        model = nn.DataParallel(model).cuda()
+
+        # this sometimes works and sometimes doesn't, no idea why
+        final_test_acc, final_train_preds, final_test_preds = train_model(model, train_loader=train_loader, test_loader=test_loader, criterion=criterion, scheduler=scheduler, optimizer=optimizer, num_epochs=num_epochs)
 
         # ensemble_train_feats = np.concatenate([bert_prob_train, style_prob_train, char_prob_train], axis=1)
         # ensemble_test_feats = np.concatenate([bert_prob_test, style_prob_test, char_prob_test], axis=1)
