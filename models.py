4edca8f8ff800da869ab5a994b5d7e868e49b294
Bo Ai <albert_0110@outlook.com>
2021-11-05 22:30:25 +0800

    added ensembles

--- models.py
+++ models.py
@@ -39,18 +39,6 @@ class MLP2Layer(nn.Module):
         return self.nn(x)
 
 
-class BertFeatExtractor(nn.Module):
-    FEAT_LEN = 768
-
-    def __init__(self, raw_bert):
-        super(BertFeatExtractor, self).__init__()
-        self.bert = raw_bert
-
-    def forward(self, x):
-        # x is a tokenized input
-        return self.bert(**x).pooler_output
-
-
 class BertClassifier(nn.Module):
     FEAT_LEN = 768
 
@@ -63,11 +51,88 @@ class BertClassifier(nn.Module):
         # x is a tokenized input
         # feature = self.bert(input_ids=x[0], token_type_ids=x[1], attention_mask=x[2])
         feature = self.bert(input_ids=x[0], attention_mask=x[2])
-        # out = self.fc(feature.pooler_output.flatten(1))       # not good for our task
-        out = self.fc(feature.last_hidden_state.flatten(1))
+        # out = self.fc(feature.pooler_output.flatten(1))       # not good for our task     # (BS, E)
+        out = self.fc(feature.last_hidden_state.flatten(1))     # (BS, T, E)
         if return_feat:
             return out, feature
         return out
+
+
+class SimpleEnsemble(nn.Module):
+    """
+    The simplest ensemble model, ie, averaging
+    """
+
+    def __init__(self, components): # components is a list of models
+        super(SimpleEnsemble, self).__init__()
+        self.components = components
+
+    def forward(self, inputs):
+        assert len(self.components) == len(inputs)
+        preds = []
+        for model, input in zip(self.components, inputs):
+            preds.append(model(input))
+        return sum(preds) / len(preds)
+
+
+class DynamicWeightEnsemble(nn.Module):
+    """
+    Learn the dynamic weights for different components
+    """
+
+    def __init__(self, components, total_feat_len, dropout=0.2, hidden_len=256):
+        super(DynamicWeightEnsemble, self).__init__()
+        self.components = components
+        self.attention = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(total_feat_len, hidden_len, bias=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_len, len(components), bias=True),
+            nn.Softmax()
+        )
+
+    def forward(self, inputs):
+        assert len(self.components) == len(inputs)
+
+        preds, feats = [], []
+        for model, input in zip(self.components, inputs):
+            pred, feat = model(input, requires_feat=True)
+            preds.append(pred)
+            feats.append(feat)
+
+        weights = self.attention(torch.cat(feats, dim=1))
+        for i, weight in enumerate(weights):
+            preds[i] = preds[i] * weight
+
+        return sum(preds)
+
+
+class AggregateFeatEnsemble(nn.Module):
+    """
+    Learn the dynamic weights for different components
+    """
+
+    def __init__(self, components, total_feat_len, num_classes, dropout=0.2, hidden_len=256):
+        super(AggregateFeatEnsemble, self).__init__()
+        self.components = components
+        self.nn = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(total_feat_len, hidden_len, bias=True),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_len, num_classes, bias=True)
+        )
+
+    def forward(self, inputs):
+        assert len(self.components) == len(inputs)
+
+        feats = []
+        for model, input in zip(self.components, inputs):
+            _, feat = model(input, requires_feat=True)
+            feats.append(feat)
+
+        return self.nn(torch.cat(feats, dim=1))
 
 
 class EnsembleClassifier(nn.Module):
