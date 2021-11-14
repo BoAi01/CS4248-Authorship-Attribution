657d21e8cab2b558f62a00a49c502b3e67add84e
Bo Ai <albert_0110@outlook.com>
2021-11-15 04:24:53 +0800

    added per auth option

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=f'Training models for datasets {datasets}')
     parser.add_argument('--dataset', type=str, help='the dataset used for training')
     parser.add_argument('--gpu', type=str, help='the cuda devices used for training', default="0,1,2,3")
+    parser.add_argument('--samples-per-author', type=int, help='number of samples per author', default=None)
     args = parser.parse_args()
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
-    list_scores = run_iterations(source=args.dataset)
+    list_scores = run_iterations(source=args.dataset, per_author=args.samples_per_author)
