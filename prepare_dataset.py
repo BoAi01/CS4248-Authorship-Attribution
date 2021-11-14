8b9ad944d59826488e9ff2625e27bfd1c53c7e97
Bo Ai <albert_0110@outlook.com>
2021-11-15 06:11:07 +0800

    cleaned up the code, fixed feat.csv file issue

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,14 +2,15 @@ import gdown
 import os
 
 datasets = {
-    'enron.csv': 'https://drive.google.com/uc?id=16Tzx6yxhsLG4gRuviI68cPIM2y3lVtjv&export=download',
-    'full_enron.csv': 'https://drive.google.com/uc?id=1y0aJLI9JeWu3Vv7lnIZbPTqZjYCIG5GY&export=download',
+    # 'enron.csv': 'https://drive.google.com/uc?id=16Tzx6yxhsLG4gRuviI68cPIM2y3lVtjv&export=download',
+    # 'full_enron.csv': 'https://drive.google.com/uc?id=1y0aJLI9JeWu3Vv7lnIZbPTqZjYCIG5GY&export=download',
     'full_imdb.csv': 'https://drive.google.com/uc?id=1bsC1DSYcg7FlmpvfrXJbIIfnBxqqKeWZ&export=download',
     # 'full_imdb_feat.csv': 'https://drive.google.com/uc?id=1Mq6Dt7m-imcP4a21a90H8IXrtaVUgUAN&export=download', # not reliable
     'imdb62.txt': 'https://drive.google.com/uc?id=1t6P6I16i4LIHx1sldIqwMYqlBALI1JbN&export=download',
     'full_imdb62.csv': 'https://drive.google.com/uc?id=1neOw0rwXzEL4g-EaqaqJ1-jIGCFhDfZn&export=download',
-    'blogtext.csv': 'https://drive.google.com/uc?id=1iZyaXG_M6vUjH1-uLSBnVAGCJyLodhgB&export=download',
-    'full_blog.csv': 'https://drive.google.com/uc?id=1V8R3ZzH_hb97EOsT5BXECcEkgGU9NBkf&export=download'
+    'full_imdb_feat.csv': 'https://drive.google.com/uc?id=1v88KYh4UI8m2rNVjXY0gXOsTFhLXU96F&export=download',
+    # 'blogtext.csv': 'https://drive.google.com/uc?id=1iZyaXG_M6vUjH1-uLSBnVAGCJyLodhgB&export=download',
+    # 'full_blog.csv': 'https://drive.google.com/uc?id=1V8R3ZzH_hb97EOsT5BXECcEkgGU9NBkf&export=download'
 }
 
 dataset_dir = 'datasets'
