corrected kl div

train.py
@@ -345,7 +345,7 @@ def train_bert(nlp_train, nlp_test, return_features=True, model_name='microsoft/
     test_x, test_y = nlp_test['content'].tolist(), nlp_test['Target'].tolist()
 
     # training setup
-    num_epochs, base_lr, base_bs, ngpus, dropout = 12, 1e-5, 4, torch.cuda.device_count(), 0.35
+    num_epochs, base_lr, base_bs, ngpus, dropout = 6, 1e-5, 6, torch.cuda.device_count(), 0.35
     num_tokens, hidden_dim, out_dim = 256, 512, max(test_y) + 1
     model = BertClassifier(extractor, LogisticRegression(embed_len * num_tokens, hidden_dim, out_dim, dropout=dropout))
     model = nn.DataParallel(model).cuda()
@@ -356,7 +356,7 @@ def train_bert(nlp_train, nlp_test, return_features=True, model_name='microsoft/
     train_set, test_set = BertDataset(train_x, train_y, tokenizer, num_tokens), \
                           BertDataset(test_x, test_y, tokenizer, num_tokens)
 
-    coefficient, temperature, batch_pos_ratio = 0.03, 0.1, 0.4
+    coefficient, temperature, batch_pos_ratio = 0.5, 0.1, 0.4
 
     # sampler = TrainSampler(train_set, batch_size=base_bs * ngpus, sim_ratio=batch_pos_ratio)
     sampler = TrainSamplerMultiClass(train_set, batch_size=base_bs * ngpus, num_classes=base_bs*ngpus//2)
@@ -383,11 +383,7 @@ def train_bert(nlp_train, nlp_test, return_features=True, model_name='microsoft/
         pg = tqdm(train_loader, leave=False, total=len(train_loader))
         for i, (x1, x2, x3, y) in enumerate(pg):
             x, y = (x1.cuda(), x2.cuda(), x3.cuda()), y.cuda()
-            try:
-                pred, feats = model(x, return_feat=True)
-            except:
-                import pdb
-                pdb.set_trace()
+            pred, feats = model(x, return_feat=True)
 
             # classification loss
             loss_1 = criterion(pred, y.long())
@@ -414,10 +410,11 @@ def train_bert(nlp_train, nlp_test, return_features=True, model_name='microsoft/
             target_matrix = torch.zeros(sim_matrix.shape).cuda()
             for i in range(target_matrix.size(0)):
                 bool_mask = (y == y[i]).type(torch.float)
-                target_matrix[i] = bool_mask / (bool_mask.sum() + 1e-8)      # normalize s.t. sum up to 1.
+                # target_matrix[i] = bool_mask / (bool_mask.sum() + 1e-8)      # normalize s.t. sum up to 1. Wrong, no need to norm!
+                target_matrix[i] = bool_mask
 
-            # contrastive loss
-            loss_2 = F.kl_div(F.softmax(sim_matrix / temperature), target_matrix)
+            # contrastive loss. for the input to kl div,
+            loss_2 = F.kl_div(F.softmax(sim_matrix / temperature).log(), target_matrix, reduction="batchmean")
 
             # total loss
             loss = coefficient * loss_1 + (1 - coefficient) * loss_2
@@ -567,7 +564,7 @@ def run_iterations(source, per_author):
     # Load data and remove emails containing the sender's name
     df = load_dataset_dataframe(source)
 
-    list_senders = [5]
+    list_senders = [10]
 
     if source == "imdb62":
         list_senders = [62]